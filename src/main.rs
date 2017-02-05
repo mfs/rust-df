542da8ddcb8c6470c35de3c96c17990055229c79
Mike Sampson <mike@sambodata.com>
2017-02-05 15:43:26 +1100

    Now filtering out fs types we don't care about.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,29 @@ extern crate nix;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::HashSet;
+use nix::sys::statvfs::vfs::Statvfs;
 
 fn main() {
     let file = File::open("/proc/mounts").unwrap();
     let reader = BufReader::new(&file);
 
+    let mut excludes = HashSet::new();
+    let exclude_types = "cgroup autofs securityfs configfs pstore binfmt_misc debugfs \
+                         hugetlbfs devpts mqueue proc sysfs";
+    for t in exclude_types.split_whitespace() {
+        excludes.insert(t);
+    }
+
     for line in reader.lines() {
         match line {
             Ok(line) => {
                 let fields: Vec<&str> = line.split_whitespace().collect();
-                println!("{} {}", fields[0], fields[1])
+                if excludes.contains(fields[2]) {
+                    continue;
+                }
+                let statvfs = Statvfs::for_path(fields[1]).unwrap();
+                println!("{} {} {} {}", fields[0], fields[1], fields[2], statvfs.f_bsize);
             },
             Err(_) => panic!(),
         }
