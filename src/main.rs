b040a3e880afdd40d8f8dbb5dd04f4b7ad3fc41e
Mike Sampson <mike@sambodata.com>
2017-02-07 22:47:16 +1100

    Remove B from Size.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use nix::sys::statvfs::vfs::Statvfs;
 
 // http://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
 fn iec(n: u64) -> String {
-    let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let units = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"];
 
     let i = (n as f64).log(1024_f64).floor() as u32;
     let p = 1024_u64.pow(i);
