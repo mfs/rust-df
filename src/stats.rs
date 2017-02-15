76bf9fe4bc2dc6bb12efd762f9c24d293b905ad7
Mike Sampson <mike@sambodata.com>
2017-02-15 22:43:34 +1100

    First attempt at sorting.
    This isn't really correct though gives the ordering I want in my setup.
    On disk filesystems, pseudo filesystems, network filesystems.

--- src/stats.rs
+++ src/stats.rs
@@ -10,12 +10,14 @@ pub struct Stats {
     pub avail: u64,
     pub percent: f64,
     pub mount: String,
+    score: usize,
 }
 
 impl Stats {
     pub fn new(fs: &str, size: u64, avail: u64, mount: &str) -> Stats {
         let used = size - avail;
         let percent = used as f64 / size as f64;
+        let score = score(fs);
         Stats {
             filesystem: shorten_lv(fs),
             size: size,
@@ -23,12 +25,32 @@ impl Stats {
             used: used,
             percent: 100.0 * percent,
             mount: mount.to_string(),
+            score: score,
         }
+    }
+}
+
+const FS_DEV: usize = 0;
+const FS_DEFAULT: usize = 500;
+const FS_NET: usize = 1000;
+
+fn score(fs: &str) -> usize {
+    if fs.contains(":") {
+        FS_NET
+    } else if fs.starts_with("/dev/") {
+        FS_DEV + fs.len()
+    } else {
+        FS_DEFAULT
     }
 }
 
 impl Ord for Stats {
     fn cmp(&self, other: &Stats) -> Ordering {
+        let cmp = self.score.cmp(&other.score);
+        if cmp != Ordering::Equal {
+            return cmp;
+        }
+
         self.filesystem.cmp(&other.filesystem)
     }
 }
